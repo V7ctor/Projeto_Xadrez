763506af9e7ae5eb9206de78eb1be7866a8abfe8
V7ctor <joaovictor89437@gmail.com>
2019-10-24 11:59:40 -0300

    Começando a Implementar as Classes Peça e Tabuleiro

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,14 +1,13 @@
 package Aplicacao;
 
 import TabuleiroCamada.Posicao;
+import TabuleiroCamada.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Posicao pos = new Posicao(3, 5);
-		
-		System.out.println(pos);
+		Tabuleiro tab = new Tabuleiro(8, 8);
 	}
 
 }
---
+++ src/TabuleiroCamada/Peca.java
@@ -0,0 +1,16 @@
+package TabuleiroCamada;
+
+public class Peca {
+
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+
+	public Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+}
